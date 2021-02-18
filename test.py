57de9387f8f9b426ddf9a76b145780fce97e990b
zekunuser <79246163+zekunuser@users.noreply.github.com>
2021-02-18 15:30:54 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+$ go get -u github.com/hguandl/dr-feeder
+$ git clone https://github.com/hguandl/dr-feeder.git
+$ cd dr-feeder
+$ go build
+$ go install
+$ dr-feeder -h
+Usage of dr-feeder:
+  -c string
+    	Configuration file (default "config.yaml")
+$ dr-feeder -c ./config.yaml
+2021/02/18 01:01:04 Waiting for post "明日方舟客户端公告"...
+2021/02/18 01:01:04 Waiting for post "微博 明日方舟Arknights"...
